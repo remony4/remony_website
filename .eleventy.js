5bd7c21a644356556aaa85498bb3d20f01d59ded
faebser <fabian.frei@esf-frei.ch>
2021-06-11 16:06:07 +0200

    Removed anchors, fixes #4

--- .eleventy.js
+++ .eleventy.js
@@ -113,21 +113,15 @@ module.exports = function(eleventyConfig) {
 
   /* Markdown Plugins */
   let markdownIt = require('markdown-it');
-  let markdownItAnchor = require('markdown-it-anchor');
   let options = {
     html: true,
     breaks: true,
     linkify: true,
   };
-  let opts = {
-    permalink: true,
-    permalinkClass: 'direct-link',
-    permalinkSymbol: '#',
-  };
 
   eleventyConfig.setLibrary(
     'md',
-    markdownIt(options).use(markdownItAnchor, opts)
+    markdownIt(options)
   );
 
   eleventyConfig.setBrowserSyncConfig({
